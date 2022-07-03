261963da5a93dd7945fe0b43ced63da6f0e5ef6f
Atsushi Nakamura <nuits.jp@live.jp>
2022-07-04 07:22:12 +0900

    dotnet-framework-desktop.ymlの追加

---
+++ Build.ps1
@@ -0,0 +1,16 @@
+$Solution_Name = "Source\FramwworkWpfApp\FramwworkWpfApp.sln"
+$Wap_Project_Path = "Source\FramwworkWpfApp\FramwworkWpfApp.Package\FramwworkWpfApp.Package.wapproj"
+$Appx_Package_Build_Mode = "StoreUpload"
+$Configuration = "Release"
+$Appx_Bundle = "Always"
+$Appx_Bundle_Platforms = "x86|x64"
+$GitHubActionsWorkflow = "Source\FramwworkWpfApp\FramwworkWpfApp.Package\FramwworkWpfApp.Package_TemporaryKey.pfx"
+
+msbuild $Solution_Name /t:Restore /p:Configuration=$Configuration
+msbuild `
+	$Wap_Project_Path `
+	/p:Configuration=$Configuration `
+	/p:UapAppxPackageBuildMode=$Appx_Package_Build_Mode `
+	/p:AppxBundle=$Appx_Bundle `
+	/p:PackageCertificateKeyFile=$GitHubActionsWorkflow `
+	/p:PackageCertificatePassword="karYwH@%4g#q9J" `
