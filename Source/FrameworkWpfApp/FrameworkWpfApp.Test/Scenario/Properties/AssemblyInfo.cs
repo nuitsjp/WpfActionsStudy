5133ae61ffe07b8754bfe25c5d94363298fc06df
Atsushi Nakamura <nuits.jp@live.jp>
2022-07-06 07:33:29 +0900

    TestAssistant Proの導入

---
+++ Source/FrameworkWpfApp/FrameworkWpfApp.Test/Scenario/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Scenario")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Scenario")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f915da76-5e24-4f44-bf54-eadff418dfa7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Source/FrameworkWpfApp/FrameworkWpfApp/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using PropertyChanged;
+
+namespace FrameworkWpfApp
+{
+    [AddINotifyPropertyChangedInterface]
+    public class MainWindowViewModel
+    {
+        public IList<string> Colors { get; } = new List<string>
+        {
+            "Red", "Blue", "Black"
+        };
+
+
+        public string SelectedColors { get; set; }
+    }
+}
