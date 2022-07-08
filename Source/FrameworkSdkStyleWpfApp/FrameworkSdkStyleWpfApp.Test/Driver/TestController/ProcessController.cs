4dc6f4217bcf4cc4202f7154ba5cb883dd71856b
Atsushi Nakamura <nuits.jp@live.jp>
2022-07-09 08:22:10 +0900

    SDK Styleの.NET Framework WPFアプリケーションの追加

---
+++ Source/FrameworkSdkStyleWpfApp/FrameworkSdkStyleWpfApp.Test/Driver/TestController/ProcessController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Codeer.Friendly.Windows;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace Driver.TestController
+{
+    public static class ProcessController
+    {
+        public static WindowsAppFriend Start()
+        {
+            //target path
+            var current = Assembly.GetExecutingAssembly().Location;
+            var repositoryPath = current.Substring(0, current.IndexOf("Source", StringComparison.Ordinal));
+            var targetPath = Path.Combine(repositoryPath, @"Source\FrameworkSdkStyleWpfApp\FrameworkSdkStyleWpfApp\bin\Debug\net48\FrameworkSdkStyleWpfApp.exe");
+            if (File.Exists(targetPath) is false)
+            {
+                throw new FileNotFoundException(targetPath);
+            }
+            var info = new ProcessStartInfo(targetPath) { WorkingDirectory = Path.GetDirectoryName(targetPath) };
+            var app = new WindowsAppFriend(Process.Start(info));
+            app.ResetTimeout();
+            return app;
+        }
+
+        public static void Kill(this WindowsAppFriend app)
+        {
+            if (app == null) return;
+
+            app.ClearTimeout();
+            try
+            {
+                Process.GetProcessById(app.ProcessId).Kill();
+            }
+            catch { }
+        }
+    }
+}
