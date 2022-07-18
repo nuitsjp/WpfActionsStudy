4818e4aec49ef8bd150919902cb5bbc522fe1372
Atsushi Nakamura <nuits.jp@live.jp>
2022-07-18 17:45:59 +0900

    update

--- Build.ps1
+++ Build.ps1
@@ -6,3 +6,18 @@ git push origin ":$tag"
 git tag -d $tag
 git tag $tag
 git push --tags
+
+# $temp = $tag.Substring($tag.IndexOf("/") + 1)
+# # Write-Host $temp
+
+# $moduleType = $temp.Substring(0, $temp.IndexOf("/"))
+# Write-Host $moduleType
+
+# $temp = $temp.Substring($temp.IndexOf("/") + 1)
+# # Write-Host $temp
+
+# $module = $temp.Substring(0, $temp.IndexOf("/"))
+# Write-Host $module
+
+# $version = $temp.Substring($temp.IndexOf("/") + 1)
+# Write-Host $version
