bafedaf74ba01b81cd70314055b81457150556f9
Atsushi Nakamura <nuits.jp@live.jp>
2022-07-18 17:24:50 +0900

    update

--- Build.ps1
+++ Build.ps1
@@ -1,18 +1,4 @@
-
-
-$Solution_Name = "Source\FramwworkWpfApp\FramwworkWpfApp.sln"
-$Wap_Project_Path = "Source\FramwworkWpfApp\FramwworkWpfApp.Package\FramwworkWpfApp.Package.wapproj"
-$Appx_Package_Build_Mode = "StoreUpload"
-$Configuration = "Release"
-$Appx_Bundle = "Always"
-$Appx_Bundle_Platforms = "x86|x64"
-$GitHubActionsWorkflow = "Source\FramwworkWpfApp\FramwworkWpfApp.Package\FramwworkWpfApp.Package_TemporaryKey.pfx"
-
-msbuild $Solution_Name /t:Restore /p:Configuration=$Configuration
-msbuild `
-	$Wap_Project_Path `
-	/p:Configuration=$Configuration `
-	/p:UapAppxPackageBuildMode=$Appx_Package_Build_Mode `
-	/p:AppxBundle=$Appx_Bundle `
-	/p:PackageCertificateKeyFile=$GitHubActionsWorkflow `
-	/p:PackageCertificatePassword="karYwH@%4g#q9J" `
+git push origin :release/0.0.1
+git tag -d release/0.0.1
+git tag release/0.0.1
+git push --tags
